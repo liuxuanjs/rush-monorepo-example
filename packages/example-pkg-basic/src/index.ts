45f8f753976ea9cce091132fb9027f18bb41ae9f
刘煊 <xuan.liu@shuyun.com>
2023-02-13 17:35:33 +0800

    test: 测试rush change

--- packages/example-pkg-basic/src/index.ts
+++ packages/example-pkg-basic/src/index.ts
@@ -1,6 +1,7 @@
 export const sum = (a: number, b: number) => {
   if ('development' === process.env.NODE_ENV) {
     console.log('development msg: boop!!!!');
+    console.log('test rush change');
   }
   return a + b;
 };
